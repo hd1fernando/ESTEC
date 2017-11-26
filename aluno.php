3929d65f1bb98014bd29e8af3ce803eab8e190ea
Lucas Alves <hi.lucasalves@gmail.com>
2017-11-26 12:42:49 -0200

    Pesquisa de alunos, clientes e turmas em aluno.html

--- aluno.php
+++ aluno.php
@@ -5,6 +5,12 @@ switch ($_POST['functionName']) {
 	case "listaAluno":
 		echo listaAluno();
         break;
+	case "listaAlunoCliente":
+		echo listaAlunoCliente();
+        break;
+	case "listaQTDAlunoTurma":
+		echo listaQTDAlunoTurma();
+        break;
 }
 
 	function listaAluno(){
@@ -17,6 +23,54 @@ switch ($_POST['functionName']) {
 		$result = array();
 		while($row = mysqli_fetch_assoc($res)){
 			$result[] = $row['id_aluno']."&".$row['nome'];
+		}
+		mysqli_free_result($res);
+		mysqli_close($conn);
+		
+		$result = array_map('utf8_encode', $result);
+		
+		echo json_encode($result);
+	}
+	
+	function listaAlunoCliente(){
+		include_once("$_SERVER[DOCUMENT_ROOT]/ESTEC/connection.php");
+
+		$idProfessor = $_POST['idProfessor'];
+		
+		$sql = "SELECT id_aluno AS 'idAluno', aluno.nome AS 'nomeAluno', aluno.telefone AS 'telefoneAluno', aluno.email AS 'emailAluno', aluno.fk_turma 'idTurma', turma.nome_turma AS 'nomeTurma',aluno. fk_cliente AS 'idCliente', cliente.nome_cliente AS 'nomeCliente'
+		FROM aluno
+		LEFT JOIN cliente ON aluno.fk_cliente = cliente.id_cliente
+		INNER JOIN turma ON aluno.fk_turma = turma.id_turma
+		INNER JOIN professor_turma ON aluno.fk_turma = professor_turma.fk_turma
+		WHERE professor_turma.fk_professor = ".$idProfessor."";
+		$res = mysqli_query($conn, $sql);
+		$result = array();
+		while($row = mysqli_fetch_assoc($res)){
+			$result[] = $row['idAluno']."&".$row['nomeAluno']."&".$row['telefoneAluno']."&".$row['emailAluno']."&".$row['idTurma']."&".$row['nomeTurma']."&".$row['idCliente']."&".$row['nomeCliente'];
+		}
+		mysqli_free_result($res);
+		mysqli_close($conn);
+		
+		$result = array_map('utf8_encode', $result);
+		
+		echo json_encode($result);
+	}
+	
+	function listaQTDAlunoTurma(){
+		include_once("$_SERVER[DOCUMENT_ROOT]/ESTEC/connection.php");
+
+		$idProfessor = $_POST['idProfessor'];
+		
+		$sql = "SELECT turma.id_turma AS 'idTurma', turma.nome_turma AS 'nomeTurma', COUNT(aluno.id_aluno) AS 'quantidade'
+				FROM aluno
+				RIGHT JOIN turma ON aluno.fk_turma = turma.id_turma
+				RIGHT JOIN professor_turma ON professor_turma.fk_turma = turma.id_turma
+				WHERE professor_turma.fk_professor = ".$idProfessor."
+				GROUP BY turma.id_turma";
+		$res = mysqli_query($conn, $sql);
+		$result = array();
+		while($row = mysqli_fetch_assoc($res)){
+			$result[] = $row['idTurma']."&".$row['nomeTurma']."&".$row['quantidade'];
 		}
 		mysqli_free_result($res);
 		mysqli_close($conn);
---
+++ cliente.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting(0);
+
+switch ($_POST['functionName']) {
+	case "listaClienteAluno":
+		echo listaClienteAluno();
+        break;
+}
+
+	function listaClienteAluno(){
+		include_once("$_SERVER[DOCUMENT_ROOT]/ESTEC/connection.php");
+		
+		$sql = "SELECT cliente.id_cliente AS 'idCliente', cliente.nome_cliente AS 'nomeCliente', cliente.telefone AS 'telefoneCliente', cliente.email AS 'emailCliente', endereco.id_endereco AS 'idEndereco', concat(endereco.logradouro,'-', endereco.numero,'-', endereco.complemento,'-',endereco.bairro,'-',endereco.cidade,'-',endereco.estado) AS 'endereco', aluno.id_aluno AS 'idAluno', aluno.nome AS 'nomeAluno'
+				FROM cliente
+				LEFT JOIN aluno ON aluno.fk_cliente = cliente.id_cliente
+				LEFT JOIN endereco ON cliente.fk_endereco = endereco.id_endereco";
+		$res = mysqli_query($conn, $sql);
+		$result = array();
+		while($row = mysqli_fetch_assoc($res)){
+			$result[] = $row['idCliente']."&".$row['nomeCliente']."&".$row['telefoneCliente']."&".$row['emailCliente']."&".$row['idEndereco']."&".$row['endereco']."&".$row['idAluno']."&".$row['nomeAluno'];
+		}
+		mysqli_free_result($res);
+		mysqli_close($conn);
+		
+		$result = array_map('utf8_encode', $result);
+		
+		echo json_encode($result);
+	}
+?>
