0b36cf83c56170358ca4b38f739fc736cda68b3e
hd1fernando <hd1.fernandosouza@hotmail.com>
2017-11-27 20:54:31 -0200

    sincronismo

--- js/estilos.js
+++ js/estilos.js
@@ -1,3 +1,10 @@
 function ola() {
     $(".menu").toggle();
+    $(".acess").css("z-index","3");
+}
+
+function show(show, hide1, hide2) {
+    $(hide1).css("background-color", "white");
+    $(show).css("background-color", "efc7ef");
+    $(hide2).css("background-color", "white");
 }
