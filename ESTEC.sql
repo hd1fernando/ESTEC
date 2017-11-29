c7ca7883630f2555eb134763974d4d0291d09fcd
hd1fernando <hd1.fernandosouza@hotmail.com>
2017-11-29 16:10:30 -0200

    detalhes em front

--- ESTEC.sql
+++ ESTEC.sql
@@ -1,131 +1,400 @@
-create database ESTEC;
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 27-Nov-2017 às 18:13
+-- Versão do servidor: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
 
-use ESTEC;
-
-create table if not exists Endereco(
-id_endereco int not null auto_increment,
-logradouro varchar(50),
-numero int,
-complemento varchar(50),
-bairro varchar(50),
-cidade varchar(50),
-estado char(2) default 'MG',
-primary key(id_endereco)
-);
-
-create table if not exists Cliente(
-id_cliente int not null auto_increment,
-nome_cliente varchar(150),
-telefone varchar(20),
-email varchar(100),
-fk_endereco int,
-primary key(id_cliente),
-foreign key(fk_endereco) references Endereco(id_endereco)
-);
-
-create table if not exists Servico(
-id_servico int not null auto_increment,
-nome_servico varchar(50),
-primary key(id_servico)
-);
-
-create table if not exists Agendamento(
-id_agendamento int not null auto_increment,
-data_agendamento date,
-hora time,
-primary key(id_agendamento)
-);
-
-create table if not exists Professor(
-id_professor int not null auto_increment,
-nome varchar(150),
-telefone varchar(20),
-email varchar(100),
-primary key(id_professor)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-create table if not exists Usuario(
-id_usuario int not null auto_increment,
-usuario varchar(20),
-senha varchar(100),
-fk_professor int,
-primary key(id_usuario),
-foreign key(fk_professor) references Professor(id_professor)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table if not exists Turma(
-id_turma int not null auto_increment,
-nome_turma varchar(50),
-primary key(id_turma)
-);
+--
+-- Database: `estec`
+--
 
-create table if not exists Professor_Turma(
-id_professor_turma int not null auto_increment,
-fk_professor int,
-fk_turma int,
-primary key(id_professor_turma),
-foreign key(fk_professor) references Professor(id_professor),
-foreign key(fk_turma) references Turma(id_turma)
-);
+-- --------------------------------------------------------
 
-create table if not exists Aluno(
-id_aluno int not null auto_increment,
-nome varchar(150),
-telefone varchar(20),
-email varchar(100),
-fk_cliente int,
-fk_turma int,
-primary key(id_aluno),
-foreign key(fk_cliente) references Cliente(id_cliente),
-foreign key(fk_turma) references Turma(id_turma)
-);
+--
+-- Estrutura da tabela `agendamento`
+--
 
+CREATE TABLE `agendamento` (
+  `id_agendamento` int(11) NOT NULL,
+  `data_agendamento` date DEFAULT NULL,
+  `hora` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
-create table if not exists Atendimento(
-id_atendimento int not null auto_increment,
-status_atendimento enum('ok','dependente'),
-feedback int,
-observacao text,
-fk_agendamento int,
-fk_servico int,
-fk_cliente int,
-fk_aluno int,
-foreign key(fk_agendamento) references Agendamento(id_agendamento),
-foreign key(fk_servico) references Servico(id_servico),
-foreign key(fk_aluno) references Aluno(id_aluno),
-foreign key(fk_cliente) references Cliente(id_cliente),
-primary key(id_atendimento)
-);
+--
+-- Extraindo dados da tabela `agendamento`
+--
 
+INSERT INTO `agendamento` (`id_agendamento`, `data_agendamento`, `hora`) VALUES
+(1, '2017-12-05', '10:30:00'),
+(2, '2017-12-06', '08:00:00');
 
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `aluno`
+--
 
+CREATE TABLE `aluno` (
+  `id_aluno` int(11) NOT NULL,
+  `nome` varchar(150) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `telefone` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `fk_cliente` int(11) DEFAULT NULL,
+  `fk_turma` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
+--
+-- Extraindo dados da tabela `aluno`
+--
 
+INSERT INTO `aluno` (`id_aluno`, `nome`, `telefone`, `email`, `fk_cliente`, `fk_turma`) VALUES
+(1, 'João Neto', '33333333', 'joao@estec.com.br', NULL, 1),
+(2, 'Mariana Noaves', '44444444', 'mariana@estec.com.br', NULL, 1);
 
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `atendimento`
+--
 
+CREATE TABLE `atendimento` (
+  `id_atendimento` int(11) NOT NULL,
+  `status_atendimento` enum('ok','dependente') CHARACTER SET latin1 DEFAULT NULL,
+  `feedback` int(11) DEFAULT NULL,
+  `observacao` text CHARACTER SET latin1,
+  `fk_agendamento` int(11) DEFAULT NULL,
+  `fk_servico` int(11) DEFAULT NULL,
+  `fk_cliente` int(11) DEFAULT NULL,
+  `fk_aluno` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
+--
+-- Extraindo dados da tabela `atendimento`
+--
 
+INSERT INTO `atendimento` (`id_atendimento`, `status_atendimento`, `feedback`, `observacao`, `fk_agendamento`, `fk_servico`, `fk_cliente`, `fk_aluno`) VALUES
+(1, 'ok', NULL, NULL, 1, 1, 1, 1);
 
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `cliente`
+--
 
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nome_cliente` varchar(150) CHARACTER SET latin1 DEFAULT NULL,
+  `telefone` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  `email` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `fk_endereco` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
+--
+-- Extraindo dados da tabela `cliente`
+--
 
+INSERT INTO `cliente` (`id_cliente`, `nome_cliente`, `telefone`, `email`, `fk_endereco`) VALUES
+(1, 'Rose', '1111111', NULL, NULL);
 
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `endereco`
+--
 
+CREATE TABLE `endereco` (
+  `id_endereco` int(11) NOT NULL,
+  `logradouro` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `complemento` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `bairro` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `cidade` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `estado` char(2) CHARACTER SET latin1 DEFAULT 'MG'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `professor`
+--
 
+CREATE TABLE `professor` (
+  `id_professor` int(11) NOT NULL,
+  `nome` varchar(150) CHARACTER SET latin1 DEFAULT NULL,
+  `telefone` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  `email` varchar(100) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
+--
+-- Extraindo dados da tabela `professor`
+--
 
+INSERT INTO `professor` (`id_professor`, `nome`, `telefone`, `email`) VALUES
+(1, 'Administrador', '31999999999', 'admin@estec.com.br');
 
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `professor_turma`
+--
 
+CREATE TABLE `professor_turma` (
+  `id_professor_turma` int(11) NOT NULL,
+  `fk_professor` int(11) DEFAULT NULL,
+  `fk_turma` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
+--
+-- Extraindo dados da tabela `professor_turma`
+--
 
+INSERT INTO `professor_turma` (`id_professor_turma`, `fk_professor`, `fk_turma`) VALUES
+(1, 1, 1),
+(2, 1, 2);
 
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `servico`
+--
 
+CREATE TABLE `servico` (
+  `id_servico` int(11) NOT NULL,
+  `nome_servico` varchar(50) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Extraindo dados da tabela `servico`
+--
+
+INSERT INTO `servico` (`id_servico`, `nome_servico`) VALUES
+(1, 'Tratamento facial'),
+(2, 'Sobrancelha'),
+(3, 'Massagem'),
+(4, 'Tratamento corporal'),
+(5, 'Tratamento capilar'),
+(6, 'Massagem relaxante'),
+(7, 'Escova'),
+(8, 'Corte de cabelo');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `turma`
+--
+
+CREATE TABLE `turma` (
+  `id_turma` int(11) NOT NULL,
+  `nome_turma` varchar(50) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Extraindo dados da tabela `turma`
+--
+
+INSERT INTO `turma` (`id_turma`, `nome_turma`) VALUES
+(1, '1A'),
+(2, '2A');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(20) CHARACTER SET latin1 DEFAULT NULL,
+  `senha` varchar(100) CHARACTER SET latin1 DEFAULT NULL,
+  `fk_professor` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `usuario`, `senha`, `fk_professor`) VALUES
+(1, 'admin', 'admin', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agendamento`
+--
+ALTER TABLE `agendamento`
+  ADD PRIMARY KEY (`id_agendamento`);
+
+--
+-- Indexes for table `aluno`
+--
+ALTER TABLE `aluno`
+  ADD PRIMARY KEY (`id_aluno`),
+  ADD KEY `fk_cliente` (`fk_cliente`),
+  ADD KEY `fk_turma` (`fk_turma`);
+
+--
+-- Indexes for table `atendimento`
+--
+ALTER TABLE `atendimento`
+  ADD PRIMARY KEY (`id_atendimento`),
+  ADD KEY `fk_agendamento` (`fk_agendamento`),
+  ADD KEY `fk_servico` (`fk_servico`),
+  ADD KEY `fk_aluno` (`fk_aluno`),
+  ADD KEY `fk_cliente` (`fk_cliente`);
+
+--
+-- Indexes for table `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD KEY `fk_endereco` (`fk_endereco`);
+
+--
+-- Indexes for table `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`id_endereco`);
+
+--
+-- Indexes for table `professor`
+--
+ALTER TABLE `professor`
+  ADD PRIMARY KEY (`id_professor`);
+
+--
+-- Indexes for table `professor_turma`
+--
+ALTER TABLE `professor_turma`
+  ADD PRIMARY KEY (`id_professor_turma`),
+  ADD KEY `fk_professor` (`fk_professor`),
+  ADD KEY `fk_turma` (`fk_turma`);
+
+--
+-- Indexes for table `servico`
+--
+ALTER TABLE `servico`
+  ADD PRIMARY KEY (`id_servico`);
+
+--
+-- Indexes for table `turma`
+--
+ALTER TABLE `turma`
+  ADD PRIMARY KEY (`id_turma`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `fk_professor` (`fk_professor`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `agendamento`
+--
+ALTER TABLE `agendamento`
+  MODIFY `id_agendamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `aluno`
+--
+ALTER TABLE `aluno`
+  MODIFY `id_aluno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `atendimento`
+--
+ALTER TABLE `atendimento`
+  MODIFY `id_atendimento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `professor`
+--
+ALTER TABLE `professor`
+  MODIFY `id_professor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `professor_turma`
+--
+ALTER TABLE `professor_turma`
+  MODIFY `id_professor_turma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `id_servico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `turma`
+--
+ALTER TABLE `turma`
+  MODIFY `id_turma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `aluno`
+--
+ALTER TABLE `aluno`
+  ADD CONSTRAINT `aluno_ibfk_1` FOREIGN KEY (`fk_cliente`) REFERENCES `cliente` (`id_cliente`),
+  ADD CONSTRAINT `aluno_ibfk_2` FOREIGN KEY (`fk_turma`) REFERENCES `turma` (`id_turma`);
+
+--
+-- Limitadores para a tabela `atendimento`
+--
+ALTER TABLE `atendimento`
+  ADD CONSTRAINT `atendimento_ibfk_1` FOREIGN KEY (`fk_agendamento`) REFERENCES `agendamento` (`id_agendamento`),
+  ADD CONSTRAINT `atendimento_ibfk_2` FOREIGN KEY (`fk_servico`) REFERENCES `servico` (`id_servico`),
+  ADD CONSTRAINT `atendimento_ibfk_3` FOREIGN KEY (`fk_aluno`) REFERENCES `aluno` (`id_aluno`),
+  ADD CONSTRAINT `atendimento_ibfk_4` FOREIGN KEY (`fk_cliente`) REFERENCES `cliente` (`id_cliente`);
+
+--
+-- Limitadores para a tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`fk_endereco`) REFERENCES `endereco` (`id_endereco`);
+
+--
+-- Limitadores para a tabela `professor_turma`
+--
+ALTER TABLE `professor_turma`
+  ADD CONSTRAINT `professor_turma_ibfk_1` FOREIGN KEY (`fk_professor`) REFERENCES `professor` (`id_professor`),
+  ADD CONSTRAINT `professor_turma_ibfk_2` FOREIGN KEY (`fk_turma`) REFERENCES `turma` (`id_turma`);
+
+--
+-- Limitadores para a tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`fk_professor`) REFERENCES `professor` (`id_professor`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # ESTEC
-Projeto desenvolvido em parceria com o curso de estética do Centro Universitário UNA.
-O ESTEC tem o propósito te auxiliar no gerenciamento de processoas e fomentar dados para os professores do curso de estética.
